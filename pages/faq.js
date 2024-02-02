6e75658a9df8a4d8f7d8479d6dcfd42e76fcb672
leandroauzier <leandroauzier02.com>
2024-02-02 13:19:28 -0300

    start

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FAQPage(){
+    return(
+        <div>
+            <h1>Alura Cases - Páginas de Perguntas</h1>
+            <Link href="/">
+                Ir para a Home
+            </Link>
+        </div>
+    )
+}
