c15ead51edc2af624fde23a61fb25d2f77c78180
leandroauzier <leandroauzier02.com>
2024-02-06 09:51:21 -0300

    adicionando pagina do faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,10 @@
+
+import FAQScreen from '../src/screens/FAQScreen';
 import PageTitle from '../src/components/Link/PageTitle';
 import { useEffect, useState } from 'react';
-import Link from '../src/components/Link';
+//import Link from '../src/components/Link';
+
+export default FAQScreen;
 
 export async function getStaticProps(){
     const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
@@ -19,27 +23,27 @@ export async function getStaticProps(){
     };
 }
 
-export default function FAQPage({ faq }) {
-    // const [faq, setFaq] = useState([]);
-    // useEffect(()=>{
+// export default function FAQPage({ faq }) {
+//     // const [faq, setFaq] = useState([]);
+//     // useEffect(()=>{
 
-    // },  []);
-    return (
-        <div>
-            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
-            <Link href="/">
-                Ir para a Home
-            </Link>
-            <ul>
-                {faq.map(({answer , question}) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+//     // },  []);
+//     return (
+//         <div>
+//             <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
+//             <Link href="/">
+//                 Ir para a Home
+//             </Link>
+//             <ul>
+//                 {faq.map(({answer , question}) => (
+//                     <li key={question}>
+//                         <article>
+//                             <h2>{question}</h2>
+//                             <p>{answer}</p>
+//                         </article>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
